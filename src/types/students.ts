696324820c004fafe4fb6a236ef8e44b8f6d6cc1
sbhalla98 <sonalibhalla98@gmail.com>
2025-07-16 23:23:18 +0530

    refactor

--- src/types/students.ts
+++ src/types/students.ts
@@ -1,4 +1,4 @@
-import { STUDENT_STATUS_FILTER } from '@/app/students/constants';
+import { STUDENT_STATUS_FILTER } from '@/constants/students';
 
 export type StudentStatusFilterType =
   (typeof STUDENT_STATUS_FILTER)[keyof typeof STUDENT_STATUS_FILTER];
